70104065b6c85a5df2d70b305a5614da7fc47929
YJZZING <yjzzing@gmail.com>
2019-11-08 21:24:56 +0900

    Add myFunction : If a document of notice collection in firestore is created, the server send message to client

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,27 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.myFunction = functions.firestore
+  .document('notice/{noticeId}')
+  .onCreate((snap, context) => { 
+  
+	const newValue = snap.data();
+	const noticeTitle = newValue.title;
+	const noticeBody = newValue.body;
+  
+    console.log('notice title:', noticeTitle, ': body :', noticeBody);
+  
+    // Notification details.
+    const payload = {
+		notification: {
+            title: noticeTitle,
+            body: noticeBody
+        }
+    };
+  
+	return admin.messaging().sendToTopic("latest_notice", payload);  
+  
+  });
